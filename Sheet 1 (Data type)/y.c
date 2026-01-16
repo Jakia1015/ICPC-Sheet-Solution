32c091f8680fbdc7c77355d08b7e346aa8683ab2
Jakia1015 <sultanajakia947@gmail.com>
2026-01-16 21:15:35 +0600

    Update y.c

--- Sheet 1 (Data type)/y.c
+++ Sheet 1 (Data type)/y.c
@@ -1,1 +1,7 @@
-
+#include <stdio.h>
+int main(){
+    long long int  A,B,C,D;
+    scanf("%lld %lld %lld %lld", &A, &B, &C, &D);
+    long long int result =((A%100)*(B%100)*(C%100)*(D%100))%100;
+    printf("%02lld\n",result);
+}
