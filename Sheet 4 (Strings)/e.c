0e3cc5bfa2d6c133b13e988d2c65c77dfdd7fe4e
Jakia1015 <sultanajakia947@gmail.com>
2026-01-16 22:21:32 +0600

    Implement string sum calculation in e.c

--- Sheet 4 (Strings)/e.c
+++ Sheet 4 (Strings)/e.c
@@ -1,1 +1,11 @@
+#include <stdio.h>
+int main() {
+    char S[1000003];
+    scanf("%s", S);
+    long long sum = 0;
+    for (int i = 0; S[i] != '\0'; i++) {
+        sum += S[i] - '0';
+    }
+    printf("%lld\n", sum);
+}
 
