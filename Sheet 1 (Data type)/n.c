627281005dcc746ed61956845b203c936e6003c8
Jakia1015 <sultanajakia947@gmail.com>
2026-01-16 21:04:40 +0600

    Update n.c

--- Sheet 1 (Data type)/n.c
+++ Sheet 1 (Data type)/n.c
@@ -1,1 +1,11 @@
-
+#include <stdio.h>
+int main(){
+    char X;
+    scanf("%c",&X);
+    if (X >= 'a' && X <= 'z'){
+        printf("%c\n",X - 32);
+    }
+    else{
+        printf("%c\n",X + 32);
+    }
+}
