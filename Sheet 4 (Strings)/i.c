058ee46172ba6ff9ba76c9ea2592cdd722cda0b0
Jakia1015 <sultanajakia947@gmail.com>
2026-01-16 22:23:27 +0600

    Update i.c

--- Sheet 4 (Strings)/i.c
+++ Sheet 4 (Strings)/i.c
@@ -1,1 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+int main(){
+    char str[1003];
+    scanf("%s", str);
+    int n = strlen(str);
+    int first = 0;
+    int last = n - 1;
+    while (first < last) {
+        if (str[first] != str[last]) {
+            printf("NO\n");
+            return 0;
+        }
+        first++;
+        last--;
+    }
+
+    printf("YES\n");
+}
 
