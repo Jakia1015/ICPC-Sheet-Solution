b73151535c94e43e34d546961a7b1ef5d7d46c06
Jakia1015 <sultanajakia947@gmail.com>
2026-01-16 21:01:48 +0600

    Update k.c

--- Sheet 1 (Data type)/k.c
+++ Sheet 1 (Data type)/k.c
@@ -1,1 +1,12 @@
+#include <stdio.h>
+int main(){
+    long long a, b, c;
+    scanf("%lld %lld %lld", &a, &b, &c);
+    long long mn = a, mx = a;
+    if (b > mx) mx =b;
+    if (b < mn) mn = b;
+    if (c > mx) mx =c;
+    if (c < mn) mn = c;
+    printf("%lld %lld\n", mn, mx);
+}
 
