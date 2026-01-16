8f80a4d67b0a7cc07896deff75e52a8c6cfe5dac
Jakia1015 <sultanajakia947@gmail.com>
2026-01-16 22:14:49 +0600

    Update o.c

--- Sheet 3 (Arrays)/o.c
+++ Sheet 3 (Arrays)/o.c
@@ -1,1 +1,15 @@
+#include <stdio.h>
+int main() {
+    int n;
+    scanf("%d", &n);
+    long long fib[n];
+    fib[0] = 0;
+    fib[1] = 1;
+    for (int i = 2; i < n; i++) {
+        fib[i] = fib[i - 1] + fib[i - 2];
+    }
 
+    printf("%lld\n", fib[n - 1]);
+
+    return 0;
+}
