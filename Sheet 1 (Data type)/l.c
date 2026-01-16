8df649d66a137f1cd85fd500e09785a05427b7d2
Jakia1015 <sultanajakia947@gmail.com>
2026-01-16 21:06:34 +0600

    Update l.c

--- Sheet 1 (Data type)/l.c
+++ Sheet 1 (Data type)/l.c
@@ -1,17 +1,13 @@
 #include <stdio.h>
 #include <string.h>
-
 int main() {
     char F1[101], S1[101];
     char F2[101], S2[101];
-
     scanf("%100s %100s", F1, S1);
     scanf("%100s %100s", F2, S2);
-
     if (strcmp(S1, S2) == 0)
         printf("ARE Brothers\n");
     else
         printf("NOT\n");
-
     return 0;
 }
