5fdef1e6560d2c8a987157687b344bd731a9382d
Jakia1015 <sultanajakia947@gmail.com>
2026-01-16 23:22:48 +0600

    Update h.c

--- Sheet 2 (Loops)/h.c
+++ Sheet 2 (Loops)/h.c
@@ -1,1 +1,24 @@
+#include <stdio.h>
+int main(){
+int X, i, isPrime = 1;
+    scanf("%d", &X);
+    if (X < 2) {
+        isPrime = 0;
+    } else {
+        for (i = 2; i <= sqrt(X); i++) {
+            if (X % i == 0) {
+                isPrime = 0;
+                break;
+            }
+        }
+    }
+
+    if (isPrime)
+        printf("YES\n");
+    else
+        printf("NO\n");
+}
+
+
+
 
