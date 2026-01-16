1f8f5dafb03dbfb344c90a5db1dca7a011614bb6
Jakia1015 <sultanajakia947@gmail.com>
2026-01-16 22:35:28 +0600

    Update b.c

--- Sheet 5 (Functions)/b.c
+++ Sheet 5 (Functions)/b.c
@@ -1,1 +1,15 @@
+#include <stdio.h>
+int printNumbers(int n) {
+    for(int i=1; i<=n; i++) {
+        printf("%d", i);
+        if(i <n)
+            printf(" ");
+    }
+    return 1;
+}
 
+int main() {
+    int n;
+    scanf("%d", &n);
+    printNumbers(n);
+}
