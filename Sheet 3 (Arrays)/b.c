37b6cdb2d708a55dd35198617647af3f53b9acc9
Jakia1015 <sultanajakia947@gmail.com>
2026-01-16 22:07:03 +0600

    Update b.c

--- Sheet 3 (Arrays)/b.c
+++ Sheet 3 (Arrays)/b.c
@@ -1,1 +1,18 @@
-
+#include <stdio.h>
+int main() {
+    int N;
+    scanf("%d", &N);
+    int A[N];
+    for(int i = 0; i < N; i++) {
+        scanf("%d", &A[i]);
+    }
+    int X;
+    scanf("%d", &X);
+    int pos = -1;
+    for (int i = 0; i < N; i++) {
+        if (A[i] == X && pos == -1) {
+            pos = i;
+        }
+    }
+    printf("%d", pos);
+}
