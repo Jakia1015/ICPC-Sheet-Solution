a165555a0fb6635077a131fe870f3b5da649c688
Jakia1015 <sultanajakia947@gmail.com>
2026-01-16 22:51:23 +0600

    Update c.c

--- Sheet 7 (Recursion)/c.c
+++ Sheet 7 (Recursion)/c.c
@@ -1,1 +1,20 @@
+#include <stdio.h>
+void solve(int i)
+{
+    if(i>=1){
+    printf("%d",i);
+    if(i>1)
+    printf(" ");
+    solve(i-1);
+    }
+
+}
+
+int main (){
+    int n ;
+
+    scanf("%d",&n);
+    solve(n);
+
+}
 
