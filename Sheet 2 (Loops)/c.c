e517883699f806a1de13d4031f1b61f5f453efdd
Jakia1015 <sultanajakia947@gmail.com>
2026-01-16 23:20:31 +0600

    Update c.c

--- Sheet 2 (Loops)/c.c
+++ Sheet 2 (Loops)/c.c
@@ -1,1 +1,24 @@
+#include <stdio.h>
+int main() {
+  int N;
+    scanf("%d", &N);
+    int even = 0, odd = 0, positive = 0, negative = 0;
+    for(int i = 0; i < N; i++) {
+        int x;
+        scanf("%d", &x);
+        if(x % 2 == 0)
+            even++;
+        else
+            odd++;
+        if(x > 0)
+            positive++;
+        else if(x < 0)
+            negative++;
+    }
+    printf("Even: %d\n", even);
+    printf("Odd: %d\n", odd);
+    printf("Positive: %d\n", positive);
+    printf("Negative: %d\n", negative);
+
+}
 
