6e3f82a2d33e09d34a71c50c30a72b671f7c7f75
Jakia1015 <sultanajakia947@gmail.com>
2026-01-16 22:33:33 +0600

    Update j.c

--- Sheet 5 (Functions)/j.c
+++ Sheet 5 (Functions)/j.c
@@ -1,1 +1,30 @@
+#include <stdio.h> 
+void average(int n, double a[]) {
+    double sum = 0.0;
+    for(int i = 0; i < n; i++) {
+        sum += a[i];
+    }
+    double average = sum/n;
+    printf("%.7f\n", average);
+ 
+}
+    
+ 
+ 
+int main() {
+    int n;
+    scanf("%d", &n);
+    double a[100000];
+ 
+    for(int i = 0; i < n; i++) {
+        scanf("%lf", &a[i]);
+    }
+ 
+     average(n,a);
+    
+ 
+}
+
+
+
 
