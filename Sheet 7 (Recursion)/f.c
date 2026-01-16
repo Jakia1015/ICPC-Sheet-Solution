1a4530980b6171ce9a62cd135d4119ec801a26a7
Jakia1015 <sultanajakia947@gmail.com>
2026-01-16 22:52:08 +0600

    Update f.c

--- Sheet 7 (Recursion)/f.c
+++ Sheet 7 (Recursion)/f.c
@@ -1,1 +1,26 @@
+#include <stdio.h>
+void printEven(long long a[], int i)
+{
+    if(i < 0)
+        return;
+    if(i % 2 == 0)
+        printf("%lld ", a[i]);
 
+    printEven(a ,i - 1);
+
+}
+int main(){
+    int n;
+    scanf("%d",&n);
+    long long a[n];
+    for(int i = 0; i < n; i++)
+       scanf("%lld",&a[i]);
+    printEven(a,n - 1);
+        return 0;
+
+
+
+
+
+
+}
