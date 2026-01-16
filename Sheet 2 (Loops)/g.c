7d3692d024777ab41913d0091ab6fb142cb64ea4
Jakia1015 <sultanajakia947@gmail.com>
2026-01-16 23:22:06 +0600

    Update g.c

--- Sheet 2 (Loops)/g.c
+++ Sheet 2 (Loops)/g.c
@@ -1,1 +1,19 @@
+#include <stdio.h>
+int main() {
+    int T;
+    scanf("%d", &T); 
+
+    for (int i = 0; i < T; i++) {
+        int N;
+        scanf("%d", &N);
+
+        long long fact = 1;
+        for (int j = 1; j <= N; j++) {
+            fact *= j;
+        }
+
+        printf("%lld\n", fact);
+    }
+}
+
 
