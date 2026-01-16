884a37cbca6005e74d34916bc02ff27ae4094549
Jakia1015 <sultanajakia947@gmail.com>
2026-01-16 21:13:58 +0600

    Update x.c

--- Sheet 1 (Data type)/x.c
+++ Sheet 1 (Data type)/x.c
@@ -1,1 +1,14 @@
+#include <stdio.h>
+int main(){
+    int l1, r1, l2, r2;
+    scanf("%d %d %d %d",&l1, &r1,&l2,&r2);
+    int start = (l1 > l2) ? l1 : l2;
+    int end = (r1 < r2) ? r1 : r2;
+    if( start <= end )
+        printf("%d %d\n",start,end);
+    else
+        printf("-1\n");
+     return 0;
+}
+
 
