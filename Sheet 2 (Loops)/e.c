c8f6be7a34b6d37ba25ec7f0c179b0defdc473cc
Jakia1015 <sultanajakia947@gmail.com>
2026-01-16 23:21:03 +0600

    Update e.c

--- Sheet 2 (Loops)/e.c
+++ Sheet 2 (Loops)/e.c
@@ -1,1 +1,13 @@
-
+#include <stdio.h>
+int main() {
+  int N;
+  scanf("%d", &N);
+   long long  num, max = 0;
+    for  (int i = 0; i < N; i++) {
+        scanf("%lld", &num);
+        if (i == 0 || num > max) {
+            max = num;
+        }
+    }
+    printf("%lld\n", max);
+}
