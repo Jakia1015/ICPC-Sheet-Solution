f6aa641e974d427f825515ac1fbb7e84dd9c31ac
Jakia1015 <sultanajakia947@gmail.com>
2026-01-16 22:22:27 +0600

    Update g.c

--- Sheet 4 (Strings)/g.c
+++ Sheet 4 (Strings)/g.c
@@ -1,1 +1,20 @@
+#include <stdio.h>
+int main() {
+    char S[100005];
+    int i = 0;
+    scanf("%s", S);
+    while (S[i] != '\0') {
+        if (S[i] == ',') {
+            S[i] = ' ';
+        }
+        else if (S[i] >= 'A' && S[i] <= 'Z') {
+            S[i] = S[i] + 32;   
+        }
+        else if (S[i] >= 'a' && S[i] <= 'z') {
+            S[i] = S[i] - 32;   
+        }
+        i++;
+    }
 
+    printf("%s\n", S);
+}
