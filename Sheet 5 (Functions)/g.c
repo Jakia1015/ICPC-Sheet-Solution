32b89d7bc78deaaf26e69277a32118f4ed89e8f2
Jakia1015 <sultanajakia947@gmail.com>
2026-01-16 22:34:34 +0600

    Update g.c

--- Sheet 5 (Functions)/g.c
+++ Sheet 5 (Functions)/g.c
@@ -1,1 +1,19 @@
+#include <stdio.h>
+void printminmax(int a[],int n)
+ {
+   int min = a[0], max = a[0];
+   for(int i=1; i<n;i++){
+        if(a[i]<min)min=a[i];
+        if(a[i]>max)max=a[i];
+   }
+   printf("%d %d", min, max);
 
+ }
+ int main(){
+    int n;
+    scanf("%d",&n);
+    int a[n];
+    for(int i=0; i<n;i++){
+    scanf("%d",&a[i]);}
+    printminmax(a,n);
+ }
